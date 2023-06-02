195e14d1934893e2eb4508d241cd24f871c59705
rsmdewis <rsmdewis2409@gmail.com>
2023-06-02 20:38:01 +0700

    updated

--- app/src/main/java/com/example/familycollection/RestApi/ApiClient.java
+++ app/src/main/java/com/example/familycollection/RestApi/ApiClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class  ApiClient {
-    public static final String BASE_URL = "http://192.168.1.5:8000/api/";
+    public static final String BASE_URL = "http://192.168.1.3:8000/api/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
