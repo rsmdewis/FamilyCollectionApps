d044178920cb545ab474dc932e8c48a47ee35078
rsmdewis <rsmdewis2409@gmail.com>
2023-06-04 12:58:32 +0700

    updated

---
+++ app/src/main/java/com/example/familycollection/activity/TransferActivity.java
@@ -0,0 +1,46 @@
+package com.example.familycollection.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.familycollection.R;
+import com.example.familycollection.adapter.AdapterProdukTransaksi;
+import com.example.familycollection.adapter.AdapterTransfer;
+import com.example.familycollection.models.ProdukTransaksi;
+import com.example.familycollection.models.Transfer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransferActivity extends AppCompatActivity {
+    RecyclerView recyclerViewTransfer;
+    Button btnBukti, btnKonfirmasi;
+    LinearLayoutManager layoutManager;
+    List<Transfer> userList;
+    AdapterTransfer adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+
+        btnBukti = (Button) findViewById(R.id.btn_bukti);
+        btnKonfirmasi = (Button) findViewById(R.id.btn_konfirmasi);
+        initData();
+
+        recyclerViewTransfer=(RecyclerView) findViewById(R.id.rv_transfer);
+        layoutManager=new LinearLayoutManager(TransferActivity.this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        adapter= new AdapterTransfer(userList);
+        recyclerViewTransfer.setLayoutManager(layoutManager);
+        recyclerViewTransfer.setAdapter(adapter);
+    }
+    private void initData() {
+        userList = new ArrayList<>();
+        userList.add(new Transfer(R.drawable.top_background1, "4 Juni 2023", "100.000"));
+        userList.add(new Transfer(R.drawable.top_background1, "4 Juni 2023", "50.000"));
+    }
+}
