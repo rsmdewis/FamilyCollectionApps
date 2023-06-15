88ce4ecdbc9cb367ecc7f282d5a36dbd36227d19
Fahrizal Azi Ferdiansyah <fahrizalaziferdiansyah@192.168.1.19>
2023-06-15 22:06:16 +0700

    updated

--- app/src/main/java/com/example/familycollection/RestApi/ApiClient.java
+++ app/src/main/java/com/example/familycollection/RestApi/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class  ApiClient {
 
 
-    public static final String BASE_URL = "http://192.168.0.13:8000/api/";
+    public static final String BASE_URL = "http://192.168.1.19:8000/api/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
